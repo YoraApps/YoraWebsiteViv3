d287ac9b0a2c1fc0480feccd558b2ff7dbc962c4
Jyoti Kumari <injkumari@Symphonysummit.com>
2018-04-08 16:10:30 +0530

    services controller added

--- application/controllers/Service/Service.php
+++ application/controllers/Service/Service.php
@@ -7,5 +7,39 @@
       public function digitalmarketing() {
         $this->load->view('service/digitalmarketing');
       }
+      public function webportaldevelopment() {
+        $this->load->view('service/webportaldevelopment');
+      }
+      public function mobileappsdevelopment() {
+        $this->load->view('service/mobileappsdevelopment');
+      }
+      public function responsivewebdesign() {
+        $this->load->view('service/responsivewebdesign');
+      }
+      public function contentwriting() {
+        $this->load->view('service/contentwriting');
+      }
+      public function webhostinganddomain() {
+        $this->load->view('service/webhostinganddomain');
+      }
+      public function webservices() {
+        $this->load->view('service/webservices');
+      }
+      public function corporatetraining() {
+        $this->load->view('service/corporatetraining');
+      }
+      public function searchengineoptimization(SEO)() {
+        $this->load->view('service/searchengineoptimization');
+      }
+      public function  searchenginemarketing(SEM)() {
+        $this->load->view('service/searchenginemarketing');
+      }
+      public function  ppcmanagementservices() {
+        $this->load->view('service/ppcmanagementservices');
+      }
+      public function   appstoreoptimization() {
+        $this->load->view('service/appstoreoptimization');
+      }
+     
    }
 ?>
