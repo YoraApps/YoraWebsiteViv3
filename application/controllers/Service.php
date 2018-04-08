6b0bd15f5749021d66666d883b401a39b91dfd49
rajendrajan2018 <rajendra.siddula@gmail.com>
2018-04-08 16:40:44 +0530

    Service menu all menulinks added

--- application/controllers/Service.php
+++ application/controllers/Service.php
@@ -40,6 +40,155 @@
       public function   appstoreoptimization() {
         $this->load->view('service/appstoreoptimization');
       }
-     
+      public function   webanalytics() {
+        $this->load->view('service/webanalytics');
+      }
+      public function   socialmediaoptimization() {
+        $this->load->view('service/socialmediaoptimization');
+      }
+      public function   onlinereputationmanagement() {
+        $this->load->view('service/onlinereputationmanagement');
+      }
+      public function   affiliatemarketing() {
+        $this->load->view('service/affiliatemarketing');
+      }
+      public function   contentmarketing() {
+        $this->load->view('service/contentmarketing');
+      }
+      public function   emailmarketing() {
+        $this->load->view('service/emailmarketing');
+      }
+      public function   smsmarketing() {
+        $this->load->view('service/smsmarketing');
+      }
+      public function   socialmediamarketing() {
+        $this->load->view('service/socialmediamarketing');
+      }
+      public function   realestateportaldevelopment() {
+        $this->load->view('service/realestateportaldevelopment');
+      }
+      public function   mobilewebsitedevelopment() {
+        $this->load->view('service/mobilewebsitedevelopment');
+      }
+      public function   webapplicationdevelopment() {
+        $this->load->view('service/webapplicationdevelopment');
+      }
+      public function   b2bb2cecommerceportal() {
+        $this->load->view('service/b2bb2cecommerceportal');
+      }
+      public function   contentmanagementsystem() {
+        $this->load->view('service/contentmanagementsystem');
+      }
+      public function   mvcarchitecture() {
+        $this->load->view('service/mvcarchitecture');
+      }
+      public function   travelportaldevelopment() {
+        $this->load->view('service/travelportaldevelopment');
+      }
+      public function   classifiedsportaldevelopment() {
+        $this->load->view('service/classifiedsportaldevelopment');
+      }
+      public function   yellowpagesportaldevelopment() {
+        $this->load->view('service/yellowpagesportaldevelopment');
+      }
+      public function   newsportaldevelopment() {
+        $this->load->view('service/newsportaldevelopment');
+      }
+      public function   jobportaldevelopment() {
+        $this->load->view('service/newsportaldevelopment');
+      }
+      public function   educationportaldevelopment() {
+        $this->load->view('service/educationportaldevelopment');
+      }
+      public function   paymentgatewayintegration() {
+        $this->load->view('service/paymentgatewayintegration');
+      }
+      public function   phonegapappsdevelopment() {
+        $this->load->view('service/phonegapappsdevelopment');
+      }
+      public function   androidappsdevelopment() {
+        $this->load->view('service/androidappsdevelopment');
+      }
+      public function   iphoneappsdevelopment() {
+        $this->load->view('service/iphoneappsdevelopment');
+      }
+
+      public function   webtemplatedesign() {
+        $this->load->view('service/webtemplatedesign');
+      }
+      public function   companylogographicsdesign() {
+        $this->load->view('service/companylogographicsdesign');
+      }
+      public function   newsletterdesign() {
+        $this->load->view('service/newsletterdesign');
+      }
+      public function   blogdesign() {
+        $this->load->view('service/blogdesign');
+      }
+      public function   customwebsitedesign() {
+        $this->load->view('service/customwebsitedesign');
+      }
+
+      public function   webcontentwriting() {
+        $this->load->view('service/webcontentwriting');
+      }
+      public function   seocontentwriting() {
+        $this->load->view('service/seocontentwriting');
+      }
+      public function   airtclewriting() {
+        $this->load->view('service/airtclewriting');
+      }
+      public function   blogposting() {
+        $this->load->view('service/blogposting');
+      }
+      public function   forumposting() {
+        $this->load->view('service/forumposting');
+      }
+      public function   websitecopywriting() {
+        $this->load->view('service/websitecopywriting');
+      }
+      public function   prereleasewriting() {
+        $this->load->view('service/prereleasewriting');
+      }
+
+      public function   windowshosting() {
+        $this->load->view('service/windowshosting');
+      }
+      public function   vpsshosting() {
+        $this->load->view('service/vpsshosting');
+      }
+      public function   dedicatedservers() {
+        $this->load->view('service/dedicatedservers');
+      }
+      public function   domainregistration() {
+        $this->load->view('service/domainregistration');
+      }
+      public function   namesuggestiontool() {
+        $this->load->view('service/namesuggestiontool');
+      }
+      public function   sslcertificates() {
+        $this->load->view('service/sslcertificates');
+      }
+      public function   whoislookup() {
+        $this->load->view('service/whoislookup');
+      }
+      public function   resellerhosting() {
+        $this->load->view('service/resellerhosting');
+      }
+      public function   emailhosting() {
+        $this->load->view('service/emailhosting');
+      }
+
+
+      public function   restfulwebservices() {
+        $this->load->view('service/restfulwebservices');
+      }
+      public function   apiservicesprovider() {
+        $this->load->view('service/apiservicesprovider');
+      }
+
+      public function   digitalmarketingcourses() {
+        $this->load->view('service/digitalmarketingcourses');
+      }
    }
 ?>
