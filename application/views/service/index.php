2f18b8fb822f8ff9ace0d8150a51c836ba543ae5
akash <aka01sh@gmail.com>
2018-04-29 16:37:10 +0530

    Menu added to al the pages

--- application/views/service/index.php
+++ application/views/service/index.php
@@ -20,9 +20,115 @@
       <div class="container">
         <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
         <a class="brand" href="./"><img src="https://www.vivantatechnologies.com/assets/images/vivanta-technologies-logo.png" alt="Web Development Company" title="Web Development Company" width="253" height="101"></a>
-        <div class="nav-collapse">
-          <ul class="nav pull-right"> <li> <a href="https://www.vivantatechnologies.com/" class="dropdown-toggle">Home</a></li><li class=" dropdown"> <a href="https://www.vivantatechnologies.com/services" class="dropdown-toggle">Services</a> <ul class="dropdown-menu"> <li class="dropdown-submenu"><a href="https://www.vivantatechnologies.com/digital-marketing">Digital Marketing</a> <ul class="digital-images dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/search-engine-optimization">Search Engine Optimization (SEO)</a></li><li><a href="https://www.vivantatechnologies.com/search-engine-marketing">Search Engine Marketing (SEM)</a></li><li><a href="https://www.vivantatechnologies.com/ppc-management-services">PPC Management Services</a></li><li><a href="https://www.vivantatechnologies.com/app-store-optimization">App Store Optimization (ASO)</a></li><li><a href="https://www.vivantatechnologies.com/web-analytics">Web Analytics</a></li><li><a href="https://www.vivantatechnologies.com/social-media-optimization">Social Media Optimization (SMO)</a></li><li><a href="https://www.vivantatechnologies.com/online-reputation-management">Online Reputation Management (ORM)</a></li><li><a href="https://www.vivantatechnologies.com/affiliate-marketing">Affiliate Marketing</a></li><li><a href="https://www.vivantatechnologies.com/content-marketing">Content Marketing</a></li><li><a href="https://www.vivantatechnologies.com/email-marketing">Email Marketing</a></li><li><a href="https://www.vivantatechnologies.com/sms-marketing">SMS Marketing</a></li><li><a href="https://www.vivantatechnologies.com/social-media-marketing">Social Media Marketing</a></li></ul> </li><li class="dropdown-submenu"><a href="https://www.vivantatechnologies.com/web-portal-development">Web Portal Development</a> <ul class="digital-images dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/real-estate-portal">Real Estate Portal Development</a></li><li><a href="https://www.vivantatechnologies.com/mobile-website-development">Mobile Website Development</a></li><li><a href="https://www.vivantatechnologies.com/web-application-development">Web Application Development</a></li><li><a href="https://www.vivantatechnologies.com/b2b-and-b2c-e-commerce-portal">B2B &amp; B2C e-Commerce Portal</a></li><li><a href="https://www.vivantatechnologies.com/content-management-system">Content Management System (CMS)</a></li><li><a href="https://www.vivantatechnologies.com/mvc-architecture">MVC Architecture</a></li><li><a href="https://www.vivantatechnologies.com/travel-portal-development">Travel Portal Development</a></li><li><a href="https://www.vivantatechnologies.com/classifieds-portal-development">Classifieds Portal Development</a></li><li><a href="https://www.vivantatechnologies.com/yellow-pages-portal-development">Yellow Pages Portal Development</a></li><li><a href="https://www.vivantatechnologies.com/news-portal-development">News Portal Development</a></li><li><a href="https://www.vivantatechnologies.com/job-portal-development">Job Portal Development</a></li><li><a href="https://www.vivantatechnologies.com/education-portal-development">Education Portal Development</a></li><li><a href="https://www.vivantatechnologies.com/payment-gateway-integration">Payment Gateway Integration</a></li><li><a href="https://www.vivantatechnologies.com/phonegap-apps-development">PhoneGap Apps Development</a></li></ul> </li><li class="dropdown-submenu"><a href="https://www.vivantatechnologies.com/mobile-apps-development">Mobile Apps Development</a> <ul class="digital-images dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/android-apps-development">Android Apps development</a></li><li><a href="https://www.vivantatechnologies.com/iphone-apps-development">iPhone Apps development</a></li></ul> </li><li class="digital-images dropdown-submenu"><a href="https://www.vivantatechnologies.com/responsive-web-design">Responsive Web Design</a> <ul class="dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/web-template-design">Web Template Design</a></li><li><a href="https://www.vivantatechnologies.com/company-logo-graphics-design">Company Logo/Graphics Design</a></li><li><a href="https://www.vivantatechnologies.com/newsletter-design">Newsletter Design</a></li><li><a href="https://www.vivantatechnologies.com/blog-design">Blog Design</a></li><li><a href="https://www.vivantatechnologies.com/custom-website-design">Custom Website Design</a></li></ul> </li><li class="dropdown-submenu"><a href="https://www.vivantatechnologies.com/content-writing">Content Writing</a> <ul class="digital-images dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/web-content-writing">Web Content Writing</a></li><li><a href="https://www.vivantatechnologies.com/seo-content-writing">SEO Content Writing</a></li><li><a href="https://www.vivantatechnologies.com/article-writing">Article Writing</a></li><li><a href="https://www.vivantatechnologies.com/blog-posting">Blog Posting</a></li><li><a href="https://www.vivantatechnologies.com/forum-posting">Forum Posting</a></li><li><a href="https://www.vivantatechnologies.com/website-copy-writing">Website Copy Writing</a></li><li><a href="https://www.vivantatechnologies.com/press-release-writing">Press Release Writing</a></li></ul> </li><li class="dropdown-submenu"><a href="https://www.vivantatechnologies.com/web-hosting-and-domain">Web Hosting &amp; Domain</a> <ul class="digital-images dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/windows-hosting">Windows Hosting</a></li><li><a href="https://www.vivantatechnologies.com/vps-hosting">VPS Hosting</a></li><li><a href="https://www.vivantatechnologies.com/dedicated-servers">Dedicated Servers</a></li><li><a href="https://www.vivantatechnologies.com/domain-registraion">Domain Registration</a></li><li><a href="https://www.vivantatechnologies.com/name-suggestion-tool">Name Suggestion Tool</a></li><li><a href="https://www.vivantatechnologies.com/ssl-certificates">SSL Certificates</a></li><li><a href="https://www.vivantatechnologies.com/whois-lookup">Whois Lookup</a></li><li><a href="https://www.vivantatechnologies.com/reseller-hosting">Reseller Hosting</a></li><li><a href="https://www.vivantatechnologies.com/email-hosting">Email Hosting</a></li></ul> </li><li class="dropdown-submenu"><a href="https://www.vivantatechnologies.com/web-services">Web Services</a> <ul class="digital-images dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/restful-services">RESTful Services</a></li><li><a href="https://www.vivantatechnologies.com/api-services-provider">API Services Provider</a></li></ul> </li><li class="dropdown-submenu"><a href="https://www.vivantatechnologies.com/corporate-training">Corporate Training</a> <ul class="digital-images dropdown-menu unstyled"> <li><a href="https://www.vivantatechnologies.com/digital-marketing-courses">Digital Marketing Courses</a></li></ul> </li></ul> </li><li> </li><li> <a href="https://www.vivantatechnologies.com/portfolio" class="dropdown-toggle">Portfolio</a> </li><li> <a href="https://www.vivantatechnologies.com/developers/">Developers</a> </li><li> <a href="https://www.vivantatechnologies.com/careers" class="dropdown-toggle">Careers</a> </li><li> <a href="https://www.vivantatechnologies.com/contact-us">Contact</a> </li></ul>        </div>
-      </div>
+  <div class="nav-collapse">
+    <ul class="nav pull-right">
+        <li><a href="https://www.yora.xyz/" class="dropdown-toggle">Home</a></li>
+        <li class=" dropdown">
+               <a href="http://www.yora.xyz/CodeIgniter/index.php/service" class="dropdown-toggle">service</a>
+            <ul class="dropdown-menu">
+                <li class="dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/digitalmarketing">Digital Marketing</a>
+                    <ul class="digital-images dropdown-menu unstyled">
+                        <li>
+                            <a href="http://www.yora.xyz/CodeIgniter/index.php/service/searchengineoptimization">Search Engine Optimization (SEO)</a>
+                        </li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/searchenginemarketing">Search Engine Marketing (SEM)</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/ppcmanagementservices">PPC Management service</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/appstoreoptimization">App Store Optimization (ASO)</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/webanalytics">Web Analytics</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/socialmediaoptimization">Social Media Optimization (SMO)</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/onlinereputationmanagement">Online Reputation Management (ORM)</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/affiliatemarketing">Affiliate Marketing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/contentmarketing">Content Marketing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/emailmarketing">Email Marketing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/smsmarketing">SMS Marketing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/socialmediamarketing">Social Media Marketing</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/webportaldevelopment">Web Portal Development</a>
+                    <ul class="digital-images dropdown-menu unstyled">
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/realestateportal">Real Estate Portal Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/mobilewebsitedevelopment">Mobile Website Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/webapplicationdevelopment">Web Application Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/b2bandb2cecommerceportal">B2B &amp; B2C eCommerce Portal</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/contentmanagementsystem">Content Management System (CMS)</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/mvcarchitecture">MVC Architecture</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/travelportaldevelopment">Travel Portal Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/classifiedsportaldevelopment">Classifieds Portal Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/yellowpagesportaldevelopment">Yellow Pages Portal Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/newsportaldevelopment">News Portal Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/jobportaldevelopment">Job Portal Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/educationportaldevelopment">Education Portal Development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/paymentgatewayintegration">Payment Gateway Integration</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/phonegapappsdevelopment">PhoneGap Apps Development</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/mobileappsdevelopment">Mobile Apps Development</a>
+                    <ul class="digital-images dropdown-menu unstyled">
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/androidappsdevelopment">Android Apps development</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/iphoneappsdevelopment">iPhone Apps development</a></li>
+                    </ul>
+                </li>
+                <li class="digital-images dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/responsivewebdesign">Responsive Web Design</a>
+                    <ul class="dropdown-menu unstyled">
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/webtemplatedesign">Web Template Design</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/companylogographicsdesign">Company Logo/Graphics Design</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/newsletterdesign">Newsletter Design</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/blogdesign">Blog Design</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/customwebsitedesign">Custom Website Design</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/content-writing">Content Writing</a>
+                    <ul class="digital-images dropdown-menu unstyled">
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/webcontentwriting">Web Content Writing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/seocontentwriting">SEO Content Writing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/articlewriting">Article Writing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/blogposting">Blog Posting</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/forumposting">Forum Posting</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/websitecopywriting">Website Copy Writing</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/pressreleasewriting">Press Release Writing</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/web-hosting-and-domain">Web Hosting &amp; Domain</a>
+                    <ul class="digital-images dropdown-menu unstyled">
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/windowshosting">Windows Hosting</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/vpshosting">VPS Hosting</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/dedicatedservers">Dedicated Servers</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/domainregistraion">Domain Registration</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/namesuggestiontool">Name Suggestion Tool</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/sslcertificates">SSL Certificates</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/whoislookup">Whois Lookup</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/resellerhosting">Reseller Hosting</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/emailhosting">Email Hosting</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/webservice">Web service</a>
+                    <ul class="digital-images dropdown-menu unstyled">
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/restfulservice">RESTful service</a></li>
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/apiserviceprovider">API service Provider</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a href="http://www.yora.xyz/CodeIgniter/index.php/service/corporatetraining">Corporate Training</a>
+                    <ul class="digital-images dropdown-menu unstyled">
+                        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/service/digitalmarketingcourses">Digital Marketing Courses</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>
+        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/portfolio" class="dropdown-toggle">Portfolio</a> </li>
+        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/developers">Developers</a> </li>
+        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/career" class="dropdown-toggle">Careers</a> </li>
+        <li><a href="http://www.yora.xyz/CodeIgniter/index.php/contactus">Contact</a> </li>
+    </ul>
+</div>
     </div>
   </div>
   <div class="container"> </div>
